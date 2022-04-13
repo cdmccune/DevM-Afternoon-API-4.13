d1f011756638a79819d7f20ce27ee6f38cb5615b
cdmccune <curtmccune@gmail.com>
2022-04-13 15:55:06 -0700

    added some functionality

--- SWAPI/main.js
+++ SWAPI/main.js
@@ -1,15 +1,12 @@
-const button = document.querySelector(`button`)
+const residentsButton = document.querySelector(`#residents`)
+const filmsButton = document.querySelector(`#films`)
+const climateButton = document.querySelector(`#climate`)
 
-buttonClick = () => {
-    // console.log("button clicked")
-    // hi = document.createElement("h2")
-    // hi.innerHTML = "ajshfdkahsf"
-    // document.body.appendChild(hi)
-
+residentsClick = () => {
     axios.get("https://swapi.dev/api/planets/?search=Alderaan", )
     .then(res => {
         for (i=0; i<res.data.results[0].residents.length; i++) {
-            console.log(res.data.results[0].residents[i])
+            console.log(res.data.results[0])
             axios.get(`${res.data.results[0].residents[i]}`)
             .then(res => {
                 console.log(res.data.name)
@@ -21,5 +18,32 @@ buttonClick = () => {
     })
 }
 
-button.addEventListener(`click`, buttonClick)
+climateClick = () => {
+    axios.get("https://swapi.dev/api/planets/?search=Alderaan", )
+    .then(res => {
+        console.log(res.data.results[0].climate)
+        let h2 = document.createElement("h2")
+        h2.innerHTML = `Alderaan is ${res.data.results[0].climate}`
+        document.body.appendChild(h2)
+    })
+}
+
+filmsClick = () => {
+    axios.get("https://swapi.dev/api/planets/?search=Alderaan", )
+    .then(res => {
+        for (i=0; i<res.data.results[0].films.length; i++) {
+            axios.get(`${res.data.results[0].films[i]}`)
+            .then(res => {
+                console.log(res.data.title)
+                let h2 = document.createElement("h2")
+                h2.innerHTML = `${res.data.title}`
+                document.body.appendChild(h2)
+            })
+        }
+    })
+}
+
+residentsButton.addEventListener(`click`, residentsClick)
+climateButton.addEventListener(`click`, climateClick)
+filmsButton.addEventListener(`click`, filmsClick)
 
